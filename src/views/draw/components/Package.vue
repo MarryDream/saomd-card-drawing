74a55bf50970a700307e40aad3c3487c8f530e3e
MarryDream <2037177696@qq.com>
2021-12-29 20:02:31 +0800

    fix: 禁止图片拖拽

--- src/views/draw/components/Package.vue
+++ src/views/draw/components/Package.vue
@@ -3,16 +3,16 @@
     <div class="result-contain">
       <div class="result-title">
         <div class="pool-title">
-          <img :src="require('@/assets/images/icon/scout.png')" alt />
+          <img :src="require('@/assets/images/icon/scout.png')" alt draggable="false" />
         </div>
       </div>
       <div class="result-pool">
         <div class="pool-contain">
           <div :key="index" v-for="(item, index) in drawList" class="card">
-            <img class="card-iframe" :src="require(`@/assets/images/chara/character_frame_${item.star}.png`)" alt />
-            <img class="card-img" :src="`https://file.uimentama.com/saomd/${item.type}_star${item.star}_job${item.job}_${item.attr}_${item.id}.png`" alt />
-            <img class="card-attr" :src="require(`@/assets/images/attribute/icon_attribute_${item.attr}.png`)" alt />
-            <img class="card-job" :src="require(`@/assets/images/job/icon_job_${item.job}.png`)" alt />
+            <img class="card-iframe" :src="require(`@/assets/images/chara/character_frame_${item.star}.png`)" alt draggable="false" />
+            <img class="card-img" :src="`https://file.uimentama.com/saomd/${item.type}_star${item.star}_job${item.job}_${item.attr}_${item.id}.png`" alt draggable="false" />
+            <img class="card-attr" :src="require(`@/assets/images/attribute/icon_attribute_${item.attr}.png`)" alt draggable="false" />
+            <img class="card-job" :src="require(`@/assets/images/job/icon_job_${item.job}.png`)" alt draggable="false" />
           </div>
         </div>
       </div>
