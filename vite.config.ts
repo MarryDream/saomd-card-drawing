421d8a7b205f29a6f9bc6780ad6cc5ef0de9c2d2
MarryDream <2037177696@qq.com>
2022-03-22 15:44:38 +0800

    build: 修改图标和根路径设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,38 @@
-import {defineConfig} from "vite"
+import {ConfigEnv, defineConfig, loadEnv} from "vite"
 import vue from "@vitejs/plugin-vue"
 import * as path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  resolve: {
-    alias: {
-      "@": path.join(__dirname, "./src")
-    }
-  },
-  server: {
-    port: 20715,
-    open: true
-  },
-  plugins: [vue()],
-  // 关闭charset，解决打包报错 [WARNING] "@charset" must be the first rule in the file
-  css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false
+export default ({mode}: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    base: env.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "./src")
+      }
+    },
+    server: {
+      port: 20715,
+      open: true
+    },
+    plugins: [vue()],
+    // 关闭charset，解决打包报错 [WARNING] "@charset" must be the first rule in the file
+    css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
       }
     }
-  }
-})
+  })
+}
